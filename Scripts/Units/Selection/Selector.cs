4fba9d1cf2dfa3ca369764e1a0d183a7da1287be
Gochomer <gastor35@gmail.com>
2024-12-20 20:30:35 +0100

    Refactored selection system and unit management and improved physics processing.
    - Implemented camera-based raycasting for mouse position retrieval.
    - Enhanced selection shape modification based on viewport input.
    - Streamlined unit addition/removal logic within the manager.
    - Introduced target movement handling for units in _PhysicsProcess.

--- Scripts/Units/Selection/Selector.cs
+++ Scripts/Units/Selection/Selector.cs
@@ -1,37 +1,52 @@
 
 using System;
+using System.Linq;
 using Godot;
 
 public partial class Selector : Area3D
 {
 
-	const float NEAR_FAR_MARGIN = .1f; // # frustum near/far planes distance from camera near/far planes
+	/// <summary>
+	/// Frustum near/far planes distance from camera near/far planes
+	/// </summary>
+	private const float NEAR_FAR_MARGIN = .1f; 
+
+	/// <summary>
+	///  Length of the ray to be cast from the viewport to calculate mouse world position
+	/// </summary>
+	private const float MOUSE_QUERY_RAY_LENGTH = 100f;
+
+	private PhysicsRayQueryParameters3D _mouseQuery;
 
 	private ReferenceRect _rect;
 	private CollisionShape3D _shape;
 
 	private Vector2 _firstPos;
 	private Vector2 _secondPos;
+	
+
+	private enum SelectionState
+	{
+		None,
+		LastCheck,
+		Selecting
+	}
+
+	private SelectionState _selectionState;
 
 	public override void _Ready()
 	{
-		
-
 		_shape = GetNode<CollisionShape3D>("CollisionShape3D");
+		_shape.Shape = new ConvexPolygonShape3D();
+
 		_rect = GetNode<ReferenceRect>("ReferenceRect");
 
 		_rect.EditorOnly = false;
 		_rect.Visible = false;
 
-		
-		// When a body enters or exists the selection shape, notified to the UnitManager
-		BodyShapeEntered += (body_rid, body, body_shape_index, local_shape_index) => {
-			UnitManager.Instance.AddUnit(body);
-		};
+		_selectionState = SelectionState.None;
 
-		BodyShapeExited += (body_rid, body, body_shape_index, local_shape_index) => {
-			UnitManager.Instance.RemoveUnit(body);
-		};
+		_mouseQuery = new();
 
 		// So that the selection it's empty at first
 		RedrawSelectionShape();
@@ -42,43 +57,7 @@ public partial class Selector : Area3D
 		base._Input(@event);
 
 		HandleSelection(@event);
-	}
-
-	static ConvexPolygonShape3D CreateFrustumCollisionMesh(Rect2 rect, Camera3D camera)
-	{
-		// Create a convex polygon collision shape
-		ConvexPolygonShape3D shape = new();
-
-		// Project 4 corners of the rect to the camera near plane
-		var pnear = ProjectSelection(rect, camera, camera.Near + NEAR_FAR_MARGIN);
-		
-		// project 4 corners of the rext to the camera far plane
-		var pfar = ProjectSelection(rect, camera, camera.Far - NEAR_FAR_MARGIN);
-
-		// create a frustum mesh from 8 projected points, 6 planes, 2 triangles per plane, 3 vertices per triangle
-		shape.Points = new Vector3[] {
-			// Near plane
-			pnear[0], pnear[1], pnear[2], 
-			pnear[1], pnear[2], pnear[3],
-			// Far plane
-			pfar[2], pfar[1], pfar[0],
-			pfar[2], pfar[0], pfar[3],
-			// Top plane
-			pnear[0], pfar[0], pfar[1],
-			pnear[0], pfar[1], pnear[1],
-			// Bottom plane
-			pfar[2], pfar[3], pnear[3],
-			pfar[2], pnear[3], pnear[2],
-			// Left plane
-			pnear[0], pnear[3], pfar[3],
-			pnear[0], pfar[3], pfar[0],
-			// Right plane
-			pnear[1], pfar[1], pfar[2],
-			pnear[1], pfar[2], pnear[2]
-		};
-
-		return shape;
-	}
+	}	
 
 	/// <summary>
 	/// Projects 4 rect corners into space, onto a viewing plane at z distance from the given camera 
@@ -88,25 +67,90 @@ public partial class Selector : Area3D
 	/// <param name="camera"></param>
 	/// <param name="z"></param>
 	/// <returns></returns>
-	static Vector3[] ProjectSelection(Rect2 rect, Camera3D camera, float z) {
-		return new Vector3[] {
+	static Vector3[] ProjectSelection(Rect2 rect, Camera3D camera, float z)
+	{
+		return [
 			camera.ProjectPosition(rect.Position, z),
 			camera.ProjectPosition(rect.Position + new Vector2(rect.Size.X, 0.0f), z),
 			camera.ProjectPosition(rect.Position + new Vector2(rect.Size.X, rect.Size.Y), z),
 			camera.ProjectPosition(rect.Position + new Vector2(0.0f, rect.Size.Y), z)
-		};
+		];
 	}
 
+	/// <summary>
+	/// Sets the shape to be a polygon based on the current selection from the viewport
+	/// </summary>
+	/// <param name="shape">The shape to modify</param>
+	/// <param name="rect"></param>
+	/// <param name="camera"></param>
+	static void ModifyFrustumCollisionMesh(Shape3D shape, Rect2 rect, Camera3D camera)
+	{
+		// Project 4 corners of the rect to the camera near plane
+		var pnear = ProjectSelection(rect, camera, camera.Near + NEAR_FAR_MARGIN);
+
+		// project 4 corners of the rect to the camera far plane
+		var pfar = ProjectSelection(rect, camera, camera.Far - NEAR_FAR_MARGIN);
+
+		if (shape is ConvexPolygonShape3D polygonShape)
+		{
+			// create a frustum mesh from 8 projected points, 6 planes, 2 triangles per plane, 3 vertices per triangle
+			polygonShape.Points = [
+				// Near plane
+				pnear[0], pnear[1], pnear[2],
+				pnear[1], pnear[2], pnear[3],
+				// Far plane
+				pfar[2], pfar[1], pfar[0],
+				pfar[2], pfar[0], pfar[3],
+				// Top plane
+				pnear[0], pfar[0], pfar[1],
+				pnear[0], pfar[1], pnear[1],
+				// Bottom plane
+				pfar[2], pfar[3], pnear[3],
+				pfar[2], pnear[3], pnear[2],
+				// Left plane
+				pnear[0], pnear[3], pfar[3],
+				pnear[0], pfar[3], pfar[0],
+				// Right plane
+				pnear[1], pfar[1], pfar[2],
+				pnear[1], pfar[2], pnear[2]
+			];
+		} else {
+			throw new Exception("The selection shape is invalid");
+		}
+	}
+
+	/// <summary>
+	/// Ensures that the selection is within limits and regenerates its shape
+	/// </summary>
 	void RedrawSelectionShape()
 	{
 		// Get frustum mesh and assign it as a collider and assign it to the area 3d
-		_rect.Size = new (
-			Mathf.Max(1, _rect.Size.X),
-			Mathf.Max(1, _rect.Size.Y)
-		);
+		_rect.Size = new(Mathf.Max(1, _rect.Size.X), Mathf.Max(1, _rect.Size.Y));
 
-		
-		_shape.Shape = CreateFrustumCollisionMesh(_rect.GetRect(), CameraManager.Instance.Camera);
+		ModifyFrustumCollisionMesh(_shape.Shape, _rect.GetRect(), CameraManager.Instance.Camera);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+
+		switch (_selectionState)
+		{
+			case SelectionState.Selecting:
+				{
+					RedrawSelectionShape();
+					UnitManager.Instance.Units = GetOverlappingBodies().ToList();
+					break;
+				}
+			case SelectionState.LastCheck:
+				{
+					RedrawSelectionShape();
+					UnitManager.Instance.Units = GetOverlappingBodies().ToList();
+					_selectionState = SelectionState.None;
+					break;
+				}
+		}
+
 	}
 
 }
--- Scripts/Units/UnitManager.cs
+++ Scripts/Units/UnitManager.cs
@@ -1,31 +1,41 @@
+using System.Collections.Generic;
 using Godot;
 
 
 
 public partial class UnitManager : Manager<UnitManager>
 {
-	private Godot.Collections.Array<Node3D> _units;
+	private List<Node3D> _units;
 
-	public Godot.Collections.Array<Node3D> Units {
+	public List<Node3D> Units {
 		get { return _units; }
-		set { _units = value; }
+		set {
+			_units = value.FindAll(unit => unit is CharacterBody3D);
+
+			if (_units.Count == 0)
+				_shouldMoveToTarget = false;
+		}
 	}
 
-	public override void _Process(double delta)
-	{
-		foreach(Node3D unit in _units)
-		{
-			// GD.Print(unit);
-		}
-		// GD.Print(_units.ToString());
+	private Vector3 _targetPosition;
+	private bool _shouldMoveToTarget;
 
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+
+		GD.Print("Loading Unit Manager");
+
+		Units = [];
 		
+		GD.Print("Loaded Unit Manager");
 	}
 
 	public void AddUnit(Node3D body)
 	{
 		GD.Print("Adding ", body, " to singleton");
-		Units.Add(body);
+		if (body is CharacterBody3D)
+			Units.Add(body);
 	}
 
 	public void RemoveUnit(Node3D body)
@@ -34,15 +44,29 @@ public partial class UnitManager : Manager<UnitManager>
 		Units.Remove(body);
 	}
 
-	public override void _EnterTree()
+	private void MoveUnitsTo(float delta)
 	{
-		base._EnterTree();
-
-		GD.Print("Loading Unit Manager");
-
-		Units = new();
-		
-		GD.Print("Loaded Unit Manager");
+		_units.ForEach(unit => {
+			unit.Position = unit.Position.MoveToward(
+				_targetPosition with { Y = unit.Position.Y },
+				delta);
+		});
 	}
+
+	public void SetUnitsTarget(Vector3 position)
+	{
+		_targetPosition = position;
+		_shouldMoveToTarget = true;
+	}
+
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+		if(_shouldMoveToTarget)
+			MoveUnitsTo((float) delta);
+    }
 
 }
