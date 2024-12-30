58944f0edd87ff5ae85ae7c6944c2a0940b133db
Francisco Esquivel Rico <franciscoesquivel032@gmail.com>
2024-12-30 19:18:20 +0100

    Started equipment system Implementations : - Partial class Inventory.Equipment - EquipAction : ItemAction - EquipmentSlot enum - Equipable : ItemData

---
+++ Scripts/Inventory/EquipmentSlot.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+/// <summary>
+/// Equipment slot enumeration
+/// Not clarified which slots are available yet
+/// </summary>
+public enum EquipmentSlot
+{
+    Head,
+    Chest,
+    Legs,
+    Feet,
+    Hands,
+    Weapon,
+    Ring1,
+    Ring2,
+    Neck,
+    Waist
+}
---
+++ Scripts/Inventory/InventoryResources/Equipable.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+/// <summary>
+/// TODO: Implement Stats
+/// </summary>
+public partial class Equipable : ItemData
+{
+    [Export]
+    public EquipmentSlot Slot { get; set; }
+    
+
+}
